3e9fa25e3e96f69c9aaf42178dee64a4e44b27db
Shaxriyar <92621920+Shaxriyar@users.noreply.github.com>
2022-01-11 09:10:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// let name ="Shaxri"
+// alert("name + " " salom) /*ogohlantirish*/
+
+// let name = prompt("What is your name: ") //O'zgaruvchi
+// let surname = prompt("What is your surname: ")
+// let age = prompt("How old are you: ")
+// let profession = prompt("Your profession: ")
+// let adress= prompt("Your adress: ")
+
+// alert ("Welcome to my veb side People," + name + " " + surname + " How old are you " + age + " " + profession + " ")
+
+// alert("O'zbekiston" `O'zbekiston`)
+
+let ism = prompt("Ismingiz nima?") //O'zgaruvchi
+let surname = prompt("Familiyangizni kiriting?")
+let age = prompt ("Yoshingiz nechida?")
+let profession = prompt("Kasbingiz?")
+let adress = prompt ("Yashash manzilingiz?")
+
+alert(`Mening ismim ${ism}, Familiyam ${surname}, Yoshim ${age}, Kasbim ${profession}, Men ${adress}, `)
